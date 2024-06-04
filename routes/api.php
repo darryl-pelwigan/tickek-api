5f9f81d6231533246ccef00182ce41d9bf82777b
Darryl Pelwigan <darrylpelwigan@gmail.com>
2024-06-04 15:34:40 +0800

    Initialize project studies

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function() {
+    return response()->json(['message' => 'Hello, API'], 200);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
