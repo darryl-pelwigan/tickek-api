738fd7814f691d1a0acf5ffbe272fed13d890b13
Darryl Pelwigan <darrylpelwigan@gmail.com>
2024-06-05 13:55:53 +0800

    Set API Resources collections

---
+++ app/Http/Resources/V1/TicketResource.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResource extends JsonResource
+{
+
+    // public static $wrap = 'ticket';
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'type' => 'ticket',
+            'id' => $this->id,
+            'attributes' => [
+                'title' => $this->title,
+                // 'description' => $this->description,
+                'description' => $this->when(
+                    $request->routeIs('tickets.show'),
+                    $this->description
+                ),
+                'status' => $this->status,
+                'createAt' => $this->created_at,
+            ],
+            'relationships' => [
+                'author' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->user_id
+                    ],
+                    'links' => [
+                        ['self' => 'todo']
+                    ]
+                ]
+            ],
+            'links' => [
+                ['self' => route('tickets.show', ['ticket' => $this->id]) ]
+            ]
+        ];
+    }
+}
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,12 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tickets', TicketController::class);
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
